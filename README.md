f4be23db67a6a88c5390c865914207e43e43b463
Iori Azuma <phazuma19980625@gmail.com>
2023-10-29 19:13:09 +0900

    MCLUV3 update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PAM_Cython
-Pachinko Allocation Model
+Pachinko Allocation Model: This repository is now under construction.
+
+## Usage
+```
+python setup.py build_ext --inplace
+```
+
+## References
+- Lin and MacCallum, 'Pachinko Allocation: DAG-Structured Mixture Models of Topic Correlations', ICML, 2006.
+- https://github.com/kenchin110100/machine_learning
--- pam_dev.py
+++ pam_dev.py
@@ -7,18 +7,26 @@ PAM dev
 @author: I.Azuma
 """
 # %%
+import pandas as pd
+import seaborn as sns
+
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.datasets import make_multilabel_classification
 
 import sys
 sys.path.append('C:/github/PAM_Cython')
-from pam_dev import pam
+from pam_cython import pam
 
 # %% PAM
-X, _ = make_multilabel_classification(random_state=0,n_samples=200,n_features=100,n_classes=10)
+n_samples = 100
+n_features = 50
+X, _ = make_multilabel_classification(random_state=0,n_samples=n_samples,n_features=n_features, n_classes=5)
+X_df = pd.DataFrame(X,index=['sample_{}'.format(i) for i in range(n_samples)],columns=['word_{}'.format(i) for i in range(n_features)])
+sns.clustermap(X_df)
 
-dat = pam_deconv.PAMDeconv(S=2,K=3,alpha0=0.01,alpha1=0.01,beta=0.1,random_state=123)
-dat.freq_df2bw(freq_df=X)
+# %%
+dat = pam.PAM(S=2,K=3,alpha0=0.01,alpha1=0.01,beta=0.1,random_state=123)
+dat.freq_df2bw(freq_df=X_df)
 dat.set_params(seed_topics=seed_topics,initial_conf=1.0)
 
 # %% ordinal LDA
